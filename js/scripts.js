db3dd2ed493714f6e18401355bd66f7fe46038c6
Jeremy Rucker <JeremyR327@gmail.com>
2022-03-16 19:02:47 -0400

    added button functionality and floodplains

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,21 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiamVyZW15cjMyNyIsImEiOiJja3poZzIxcGgwcG9vMm5xcnY2NmZpcmtiIn0.Psz_nx06Mc4XL7qDq1zwVg'
 
 // lngLat for the fountain in Washington Square Park
-var wspCenter = [-73.907456, 40.720831]
+var wspCenter = [-73.957456, 40.710831]
 
 var theData;
+
+var dummy_geojson = {
+  "type": "Feature",
+  "properties": {},
+  "geometry": {
+    "type": "Point",
+    "coordinates": [
+      -13.7109375,
+      34.88593094075317
+    ]
+  }
+}
 
 $.getJSON('./data/svi.geojson', function(rawData) {
   console.log( "Data Loaded" );
@@ -15,8 +27,8 @@ $.getJSON('./data/svi.geojson', function(rawData) {
 $('#reset-map').on('click', function() {
   // when this is clicked, let's fly the map to Midtown Manhattan
   map.flyTo({
-    center: [-73.907456, 40.720831],
-    zoom: 9.5
+    center: wspCenter,
+    zoom: 9.7
   })
 })
 
@@ -25,10 +37,11 @@ var map = new mapboxgl.Map({
   container: 'mapContainer', // HTML container id
   style: 'mapbox://styles/mapbox/dark-v9', // style URL
   center: wspCenter, // starting position as [lng, lat]
-  zoom: 9.5,
+  zoom: 9.7,
   minZoom: 8,
   maxZoom: 14
 });
+map.addControl(new mapboxgl.NavigationControl());
 
 map.on('load', function() {
   map.addSource('sensors', {
@@ -36,6 +49,27 @@ map.on('load', function() {
     // Use a URL for the value for the `data` property.
     data: './data/sensorLocations.geojson'
   });
+
+  map.addSource('2020_100yr', {
+    type: 'geojson',
+    // Use a URL for the value for the `data` property.
+    data: './data/SLR_2020s_100yr_floodplain.geojson'
+  });
+
+  map.addLayer({
+    'id': '2020_100yr-fill',
+    'type': 'fill',
+    'source': '2020_100yr',
+    'paint': {
+      'fill-color': 'lightblue',
+      'fill-opacity': 0.3,
+      'fill-outline-color': '#ccc',
+    }
+  });
+
+  map.setLayoutProperty('2020_100yr-fill', 'visibility', 'none');
+  map.moveLayer('2020_100yr-fill');
+
 
   map.addSource('svi', {
     type: 'geojson',
@@ -154,45 +188,11 @@ map.on('load', function() {
     }
   });
 
-  map.addSource('hightide', {
-    type: 'geojson',
-    // Use a URL for the value for the `data` property.
-    data: './data/futurehightide.geojson'
-  });
-
-  map.addLayer({
-    'id': 'hightide-fill',
-    'type': 'fill',
-    'source': 'hightide',
-    'paint': {
-      'fill-color': 'lightblue',
-      'fill-opacity': 0.3,
-      'fill-outline-color': '#ccc',
-    }
-  });
-
     map.setLayoutProperty('sviTheme1-fill', 'visibility', 'none');
     map.setLayoutProperty('sviTheme2-fill', 'visibility', 'none');
     map.setLayoutProperty('sviTheme3-fill', 'visibility', 'none');
     map.setLayoutProperty('sviTheme4-fill', 'visibility', 'none');
   //
-  // map.setPaintProperty('nyu-study-area-fill', 'fill-color', [
-  //   'match',
-  //   ['get', 'LandUse'],
-  //   '01', '#f4f455',
-  //   '02', '#f7d496',
-  //   '03', '#FF9900',
-  //   '04', '#f7cabf',
-  //   '05', '#ea6661',
-  //   '06', '#d36ff4',
-  //   '07', '#dac0e8',
-  //   '08', '#5CA2D1',
-  //   '09', '#8ece7c',
-  //   '10', '#bab8b6',
-  //   '11', '#5f5f60',
-  //   '12', '#5f5f60',
-  //   /* other */ '#000'
-  // ]);
 
   map.on('mouseenter', 'sviTheme1-fill', function(e) {
     // Change the cursor style as a UI indicator.
@@ -284,9 +284,9 @@ map.on('load', function() {
     }
 
     var popupContent = `
-      <h2>${street}</h2>
+      <h4>${street}</h4>
         <p><strong>Neighborhood:</strong> ${nhood}</p>
-        <p><strong>Mounted Over:</strong> ${mountTo}</p>
+        <p><strong>Mounted To:</strong> ${mountTo}</p>
         <p><strong>Mounted Over:</strong> ${mountOver}</p>
     `
 
@@ -295,71 +295,80 @@ map.on('load', function() {
     popup.setLngLat(coordinates).setHTML(popupContent).addTo(map);
   });
 
+  var reset_highlight = function() {
+    console.log('resetting!')
+    map.getSource('selected-feature').setData(dummy_geojson)
+    console.log(map.getSource('selected-feature'))
+    $('#sidebar-content-area').empty()
+  }
+
+  $('.btn-primary').on('click', function() {
+    reset_highlight()
+    map.setLayoutProperty('sviTheme1-fill', 'visibility', 'none');
+    map.setLayoutProperty('sviTheme2-fill', 'visibility', 'none');
+    map.setLayoutProperty('sviTheme3-fill', 'visibility', 'none');
+    map.setLayoutProperty('sviTheme4-fill', 'visibility', 'none');
+  });
+
   $('#toggle-theme1').on('click', function() {
+    reset_highlight()
     var visibility = map.getLayoutProperty('sviTheme1-fill', 'visibility');
-    if (visibility === 'none') {
-      map.setLayoutProperty('sviTheme1-fill', 'visibility', 'visible');
-      map.setLayoutProperty('sviTheme2-fill', 'visibility', 'none');
-      map.setLayoutProperty('sviTheme3-fill', 'visibility', 'none');
-      map.setLayoutProperty('sviTheme4-fill', 'visibility', 'none');
-    }
-    else {
-      map.setLayoutProperty('sviTheme1-fill', 'visibility', 'none');
-    }
+    map.setLayoutProperty('sviTheme1-fill', 'visibility', 'visible');
+    map.setLayoutProperty('sviTheme2-fill', 'visibility', 'none');
+    map.setLayoutProperty('sviTheme3-fill', 'visibility', 'none');
+    map.setLayoutProperty('sviTheme4-fill', 'visibility', 'none');
 });
 
   $('#toggle-theme2').on('click', function() {
+    reset_highlight()
     var visibility = map.getLayoutProperty('sviTheme2-fill', 'visibility');
-    if (visibility === 'none') {
-      map.setLayoutProperty('sviTheme2-fill', 'visibility', 'visible');
-      map.setLayoutProperty('sviTheme1-fill', 'visibility', 'none');
-      map.setLayoutProperty('sviTheme3-fill', 'visibility', 'none');
-      map.setLayoutProperty('sviTheme4-fill', 'visibility', 'none');
-    }
-    else {
-      map.setLayoutProperty('sviTheme2-fill', 'visibility', 'none');
-    }
+    map.setLayoutProperty('sviTheme2-fill', 'visibility', 'visible');
+    map.setLayoutProperty('sviTheme1-fill', 'visibility', 'none');
+    map.setLayoutProperty('sviTheme3-fill', 'visibility', 'none');
+    map.setLayoutProperty('sviTheme4-fill', 'visibility', 'none');
   });
 
   $('#toggle-theme3').on('click', function() {
+    reset_highlight()
     var visibility = map.getLayoutProperty('sviTheme3-fill', 'visibility');
-    if (visibility === 'none') {
       map.setLayoutProperty('sviTheme3-fill', 'visibility', 'visible');
       map.setLayoutProperty('sviTheme1-fill', 'visibility', 'none');
       map.setLayoutProperty('sviTheme2-fill', 'visibility', 'none');
       map.setLayoutProperty('sviTheme4-fill', 'visibility', 'none');
-    }
-    else {
-      map.setLayoutProperty('sviTheme3-fill', 'visibility', 'none');
-    }
   });
 
   $('#toggle-theme4').on('click', function() {
+    reset_highlight()
     var visibility = map.getLayoutProperty('sviTheme4-fill', 'visibility');
-    if (visibility === 'none') {
       map.setLayoutProperty('sviTheme4-fill', 'visibility', 'visible');
       map.setLayoutProperty('sviTheme1-fill', 'visibility', 'none');
       map.setLayoutProperty('sviTheme2-fill', 'visibility', 'none');
       map.setLayoutProperty('sviTheme3-fill', 'visibility', 'none');
-    }
-    else {
-      map.setLayoutProperty('sviTheme4-fill', 'visibility', 'none');
-    }
   });
 
-  $('#toggle-flood').on('click', function() {
-    var visibility = map.getLayoutProperty('hightide-fill', 'visibility');
-    if (visibility === 'none') {
-      map.setLayoutProperty('hightide-fill', 'visibility', 'visible');
-    }
-    else {
-      map.setLayoutProperty('hightide-fill', 'visibility', 'none');
-    }
+  $('#2020s-btn').on('click', function() {
+    var visibility = map.getLayoutProperty('2020_100yr-fill', 'visibility');
+      map.setLayoutProperty('2020_100yr-fill', 'visibility', 'visible');
   });
 
 // Highlight and Select Census Track
 // initialize a source with dummy data
   map.addSource('selected-feature', {
+    type: 'geojson',
+    data: dummy_geojson
+  });
+
+  map.addLayer({
+    id: 'selected-feature-fill',
+    type: 'fill',
+    source: 'selected-feature',
+    paint: {
+      'fill-opacity': 0.5,
+      'fill-color': 'yellow'
+    }
+  });
+
+  map.addSource('hover-feature', {
     type: 'geojson',
     data: {
       "type": "Feature",
@@ -375,33 +384,62 @@ map.on('load', function() {
   });
 
   map.addLayer({
-    id: 'selected-feature-fill',
+    id: 'hover-feature-fill',
     type: 'fill',
-    source: 'selected-feature',
+    source: 'hover-feature',
     paint: {
       'fill-opacity': 0.5,
-      'fill-color': 'yellow'
+      'fill-color': 'navy'
     }
   });
 
-  map.addLayer({
-    id: 'selected-feature-line',
-    type: 'line',
-    source: 'selected-feature',
-    paint: {
-      'line-color': 'gray',
-      'line-width': 2,
-      'line-opacity':0.5,
-      'line-dasharray': [1, 1]
-    }
+  $(".list-group .list-group-item").click(function(e) {
+   $(".list-group .list-group-item").removeClass("active");
+   $(e.target).addClass("active");
   });
 
+  $(".btn-primary").click(function(e) {
+   $(".list-group .list-group-item").removeClass("active");
+  });
+
+  $(function() {
+    var btn = $(".btn-primary");
+    var toggled = false;
+    btn.on("click", function() {
+        if(!toggled)
+        {
+          toggled = true;
+          btn.text("Hide Census Tract SVI Layers");
+        } else {
+          toggled = false;
+          btn.text("Show Census Tract SVI Layers");
+        }
+    });
+});
+
+$(".btn-flood").click(function(e) {
+ map.setLayoutProperty('2020_100yr-fill', 'visibility', 'none');
+ $(".flood-group .btn-check").removeClass("active");
+});
+
+$(function() {
+  var btn = $(".btn-flood");
+  var toggled = false;
+  btn.on("click", function() {
+      if(!toggled)
+      {
+        toggled = true;
+        btn.text("Hide Floodplain Layers");
+      } else {
+        toggled = false;
+        btn.text("Show Floodplain Layers");
+      }
+  });
+});
 
   map.on('click', function(e) {
     var features = map.queryRenderedFeatures(e.point)
     var featureOfInterestProperties = features[0].properties
-
-
 
     var censusTract = featureOfInterestProperties['LOCATION']
     // look up the feature in cleanData that matches this boro_cd code
@@ -412,6 +450,25 @@ map.on('load', function() {
     console.log('the geometry', featureOfInterestGeometry)
     // use this geometry to update the source for the selected layer
     map.getSource('selected-feature').setData(featureOfInterestGeometry)
+
+    var tract = featureOfInterestProperties["LOCATION"].split(',').slice(0,2)
+    var pop = featureOfInterestProperties["E_TOTPOP"]
+    var theme1 = featureOfInterestProperties['RPL_THEME1']
+    var theme2 = featureOfInterestProperties['RPL_THEME2']
+    var theme3 = featureOfInterestProperties['RPL_THEME3']
+    var theme4 = featureOfInterestProperties['RPL_THEME4']
+
+    $('#sidebar-content-area').html(`
+      <h4>${tract}</h4>
+      <p><strong>2010 Population:</strong> ${pop}</p>
+      <p><strong>Socieconomic Status</strong>: ${theme1}</p>
+      <p><strong>Household Composition & Disability:</strong> ${theme2}</p>
+      <p><strong>Minority Status and Language:</strong> ${theme3}</p>
+      <p><strong>Housing Type & Transportation:</strong> ${theme4}</p>
+    `)
+
   });
+
+
 
 })
